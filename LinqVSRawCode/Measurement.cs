b7b736ca19733e710c339751684fdfd097148067
Alex Yakunin <alex.yakunin@gmail.com>
2012-07-18 15:02:43 +0600

    Better measurements.

--- LinqVSRawCode/Measurement.cs
+++ LinqVSRawCode/Measurement.cs
@@ -3,45 +3,60 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace LinqVSRawCode
 {
-    public class Measurement : IDisposable
+    public static class Measurement
     {
-        public string Title { get; set; }
-        public bool IsBaseline { get; set; }
-        private Stopwatch stopwatch;
+        private const int DefaultTryCount = 10;
         private static Stopwatch baseline;
 
-        public Measurement(string title)
+        public static void Run(string title, Func<object> action)
         {
-            for (int i = 0; i < 5; i++) {
-                GC.WaitForPendingFinalizers();
-                GC.GetTotalMemory(true);
+            Run(title, false, DefaultTryCount, action);
+        }
+
+        public static void Run(string title, int tryCount, Func<object> action)
+        {
+            Run(title, false, tryCount, action);
+        }
+
+        public static void Run(string title, bool isBaseline, Func<object> action)
+        {
+            Run(title, isBaseline, DefaultTryCount, action);
+        }
+
+        public static void Run(string title, bool isBaseline, int tryCount, Func<object> action)
+        {
+            Console.Write("  {0,-17} ", title + ":");
+            object result = null;
+            Stopwatch bestTimer = null;
+            for (int i = 0; i < tryCount; i++) {
+                for (int j = 0; j < 5; j++) {
+                    Thread.Sleep(10);
+                    GC.WaitForPendingFinalizers();
+                    GC.GetTotalMemory(true);
+                }
+                var timer = new Stopwatch();
+                timer.Start();
+                result = action.Invoke();
+                timer.Stop();
+                if (bestTimer==null || bestTimer.Elapsed > timer.Elapsed)
+                    bestTimer = timer;
             }
-            Title = title;
-            Console.Write("{0,-17} ", title + ":");
-            stopwatch = new Stopwatch();
-            stopwatch.Start();
-        }
-
-        public Measurement SetBaseline(bool isBaseline = true)
-        {
-            IsBaseline = isBaseline;
-            return this;
-        }
-
-        public void Dispose()
-        {
-            stopwatch.Stop();
-            Console.Write("{0,7:F2}ms", stopwatch.Elapsed.TotalMilliseconds);
-            if (IsBaseline) {
-                baseline = stopwatch;
+            Console.Write("{0,7:F2}ms", bestTimer.Elapsed.TotalMilliseconds);
+            if (isBaseline) {
+                baseline = bestTimer;
                 Console.Write(", baseline");
             }
             else if (baseline!=null)
-                Console.Write(", x{0,5:F2}", (stopwatch.Elapsed.TotalMilliseconds / baseline.Elapsed.TotalMilliseconds));
+                Console.Write(", x{0,5:F2}", (bestTimer.Elapsed.TotalMilliseconds / baseline.Elapsed.TotalMilliseconds));
             Console.WriteLine();
+            if (result is IEnumerable<int> && !(result is List<int>))
+                Console.WriteLine("    Enumerator:    {0}", (result as IEnumerable<int>).GetEnumerator().GetType().Name);
+            else if (result is IEnumerable<string>)
+                Console.WriteLine("    Enumerator:    {0}", (result as IEnumerable<string>).GetEnumerator().GetType().Name);
         }
     }
 }
--- LinqVSRawCode/Test.cs
+++ LinqVSRawCode/Test.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace LinqVSRawCode
 {
@@ -11,24 +9,12 @@ namespace LinqVSRawCode
     {
         static void Main(string[] args)
         {
-            new Test().RunWithWarmup(int.Parse(args.FirstOrDefault() ?? "1000000"));
-        }
-
-        private void RunWithWarmup(int iterationCount)
-        {
-            Console.WriteLine("Iteration count: {0}", iterationCount);
-            Console.WriteLine("Warmup...");
-            var standardOut = Console.Out;
-            Console.SetOut(TextWriter.Null);
-            Run(Math.Max(Math.Min(1000000, iterationCount), iterationCount / 10));
-
-            Console.SetOut(standardOut);
-            Console.WriteLine("  Done.");
-            Run(iterationCount);
+            new Test().Run(int.Parse(args.FirstOrDefault() ?? "1000000"));
         }
 
         private void Run(int iterationCount)
         {
+            Console.WriteLine("Iteration count: {0}", iterationCount);
             var rnd = new Random();
             int max = 1000;
             int border = max / 2;
@@ -37,149 +23,188 @@ namespace LinqVSRawCode
                 .ToArray();
             var list = array.ToList();
             var sequence = new ArrayToSequenceWrapper<int>(array);
-            IEnumerable<int> resultSequence;
             var set = new HashSet<int>(array.Take(border).Distinct());
 
             Console.WriteLine("\r\nArray.Where():");
-            using (new Measurement("  For loop").SetBaseline()) {
-                for (int i = 0; i < array.Length; i++) {
-                    int v = array[i];
+            Measurement.Run("For loop", true, () => {
+                var src = array;
+                int result = 0;
+                for (int i = 0; i < src.Length; i++) {
+                    int v = src[i];
                     if (v < border)
-                        Noop(v);
+                        result = v;
                 }
-            }
-            using (new Measurement("  Foreach loop")) {
-                foreach (var v in array) {
+                return (object) result;
+            });
+            Measurement.Run("Foreach loop", () => {
+                var src = array;
+                int result = 0;
+                foreach (var v in src) {
                     if (v < border)
-                        Noop(v);
+                        result = v;
                 }
-            }
-            using (new Measurement("  LINQ")) {
-                resultSequence = array.Where(v => v < border);
-                foreach (var item in resultSequence)
-                    Noop(item);
-            }
-            DumpEnumeratorType(resultSequence);
+                return (object) result;
+            });
+            Measurement.Run("LINQ", () => {
+                var src = array;
+                var result = src.Where(v => v < border);
+                foreach (var item in result);
+                return result;
+            });
 
             Console.WriteLine("\r\nArray.Where().Select():");
-            using (new Measurement("  For loop").SetBaseline()) {
-                for (int i = 0; i < array.Length; i++) {
-                    int v = array[i];
+            Measurement.Run("For loop", true, () => {
+                var src = array;
+                int result = 0;
+                for (int i = 0; i < src.Length; i++) {
+                    int v = src[i];
                     if (v < border)
-                        Noop(v + 1);
+                        result = v + 1;
                 }
-            }
-            using (new Measurement("  Foreach loop")) {
-                foreach (var v in array) {
+                return (object) result;
+            });
+            Measurement.Run("Foreach loop", () => {
+                var src = array;
+                int result = 0;
+                foreach (var v in src) {
                     if (v < border)
-                        Noop(v + 1);
+                        result = v + 1;
                 }
-            }
-            using (new Measurement("  LINQ")) {
-                resultSequence =
-                    from v in array
+                return (object) result;
+            });
+            Measurement.Run("LINQ", () => {
+                var src = array;
+                var result =
+                    from v in src
                     where v < border
                     select v + 1;
-                foreach (var item in resultSequence)
-                    Noop(item);
-            }
-            DumpEnumeratorType(resultSequence);
+                foreach (var item in result);
+                return result;
+            });
 
             Console.WriteLine("\r\nArray.Where().Select().ToList():");
-            using (new Measurement("  For loop").SetBaseline()) {
+            Measurement.Run("For loop", true, () => {
+                var src = array;
                 var result = new List<int>();
-                for (int i = 0; i < array.Length; i++) {
-                    int v = array[i];
+                for (int i = 0; i < src.Length; i++) {
+                    int v = src[i];
                     if (v < border)
                         result.Add(v + 1);
                 }
-            }
-            using (new Measurement("  Foreach loop")) {
+                return result;
+            });
+            Measurement.Run("Foreach loop", () => {
+                var src = array;
                 var result = new List<int>();
-                foreach (var v in array) {
+                foreach (var v in src) {
                     if (v < border)
                         result.Add(v + 1);
                 }
-            }
-            using (new Measurement("  LINQ")) {
-                resultSequence = 
-                    from v in array
+                return (object) result;
+            });
+            Measurement.Run("LINQ", () => {
+                var src = array;
+                var result = 
+                    from v in src
                     where v < border
                     select v + 1;
-                var result = resultSequence.ToList();
-            }
-            DumpEnumeratorType(resultSequence);
+                result.ToList();
+                return result;
+            });
 
             Console.WriteLine("\r\nList.Where().Select().ToList():");
-            using (new Measurement("  For loop").SetBaseline()) {
+            Measurement.Run("For loop", true, () => {
+                var src = list;
                 var result = new List<int>();
-                var count = list.Count;
+                var count = src.Count;
                 for (int i = 0; i < count; i++) {
-                    int v = list[i];
+                    int v = src[i];
                     if (v < border)
                         result.Add(v + 1);
                 }
-            }
-            using (new Measurement("  Foreach loop")) {
+                return result;
+            });
+            Measurement.Run("Foreach loop", () => {
+                var src = list;
                 var result = new List<int>();
-                foreach (var v in list) {
+                foreach (var v in src) {
                     if (v < border)
                         result.Add(v + 1);
                 }
-            }
-            using (new Measurement("  LINQ")) {
-                resultSequence = 
-                    from v in list
+                return result;
+            });
+            Measurement.Run("LINQ", () => {
+                var src = list;
+                var result = 
+                    from v in src
                     where v < border
                     select v + 1;
-                var result = resultSequence.ToList();
-            }
-            DumpEnumeratorType(resultSequence);
+                result.ToList();
+                return result;
+            });
 
             Console.WriteLine("\r\nSequence.Where().Select().ToList():");
-            using (new Measurement("  Foreach loop").SetBaseline()) {
+            Measurement.Run("Foreach loop", true, () => {
+                var src = sequence;
                 var result = new List<int>();
-                foreach (var v in sequence) {
+                foreach (var v in src) {
                     if (v < border)
                         result.Add(v + 1);
                 }
-            }
-            using (new Measurement("  LINQ")) {
-                resultSequence = 
-                    from v in sequence
+                return result;
+            });
+            Measurement.Run("LINQ", () => {
+                var src = sequence;
+                var result = 
+                    from v in src
                     where v < border
                     select v + 1;
-                var result = resultSequence.ToList();
-            }
-            DumpEnumeratorType(resultSequence);
+                result.ToList();
+                return result;
+            });
 
             Console.WriteLine("\r\nSequence.Where(HashSet.Contains).Select().ToList():");
-            using (new Measurement("  Foreach loop").SetBaseline()) {
+            Measurement.Run("Foreach loop", true, () => {
+                var src = sequence;
                 var result = new List<int>();
-                foreach (var v in sequence) {
+                foreach (var v in src) {
                     if (set.Contains(v))
                         result.Add(v + 1);
                 }
-            }
-            using (new Measurement("  LINQ")) {
-                resultSequence = 
-                    from v in sequence
+                return result;
+            });
+            Measurement.Run("LINQ", () => {
+                var src = sequence;
+                var result = 
+                    from v in src
                     where set.Contains(v)
                     select v + 1;
-                var result = resultSequence.ToList();
-            }
-            DumpEnumeratorType(resultSequence);
+                result.ToList();
+                return result;
+            });
+
+            Console.WriteLine("\r\nArray.Select(StringBuilder.Append(int.ToString)):");
+            Measurement.Run("Foreach loop", true, () => {
+                var src = array;
+                var result = new StringBuilder();
+                foreach (var v in src)
+                    result.Append(v.ToString());
+                return result;
+            });
+            Measurement.Run("LINQ", () => {
+                var src = array;
+                var result = src.Select(i => i.ToString());
+                var sb = new StringBuilder();
+                foreach (var item in result)
+                    sb.Append(item);
+                return result;
+            });
         }
 
-        private static void DumpEnumeratorType(IEnumerable<int> resultSequence)
+        private static void Noop(bool value = false)
         {
-            Console.WriteLine("    Enumerator:    {0}", resultSequence.GetEnumerator().GetType().Name);
-        }
-
-        private static void Noop(int item)
-        {
-            if (item==int.MinValue)
-                Noop(item);
+            if (value)
+                Noop(value);
         }
     }
 }
