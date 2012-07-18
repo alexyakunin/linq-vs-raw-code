c6fb0795723af6fc95bf9ffde7cc635f8eb76959
Alex Yakunin <alex.yakunin@gmail.com>
2012-07-18 20:32:25 +0600

    Output is updated.

--- RunAll.bat
+++ RunAll.bat
@@ -1,6 +1,6 @@
 @echo off
-call :run 20 "20 items"
-call :run 100 "100 items"
+call :run 20 "20 items" true
+call :run 100  "100 items"
 call :run 10000 "10K items"
 call :run 1000000 "1M items"
 call :run 50000000 "50M items (200+ MB RAM, so working set doesn't fit into L2)"
@@ -9,7 +9,7 @@ goto :eof
 :run
 echo Output for %~2:
 echo ^<pre^>
-call Run.bat %1
+call Run.bat %1 %3
 echo ^</pre^>
 echo.
 goto :eof
